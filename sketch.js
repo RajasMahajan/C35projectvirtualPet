adbd80aad3839d731549dd07ed953bc8513645d5
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2020-11-27 11:18:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,20 +4,23 @@ var dog;
 var dog1;
 var dog2;
 var foodstock;
+var foodS;
+var e;
 function preload()
 {
   //load images here
   dog1=loadImage("dogImg.png");
-  dog2=loadImage("dogImg.png");
+  dog2=loadImage("dogImg1.png");
 }
 
 function setup() {
 	createCanvas(500,800);
   dog=createSprite(250,600,30,30);
-  dog.addImage(dog1)
+  dog.scale=0.7;
+  dog.addImage(dog1);
   database=firebase.database();
-  foodstock = database.ref('virtualpet/food');
-  foodstock.on("value",readstock);
+  foodS = database.ref('food');
+  foodS.on("value",readstock);
 }
 
 
@@ -25,15 +28,27 @@ function draw() {
 
   drawSprites();
   //add styles here
-  if(keyWentDown(key_down)){
-    writestock(foodS);
-    dog.addImage(dog2);
-  }
+  
+    if(keyWentDown(UP_ARROW)){
+      writestock(foodS);
+      dog.addImage(dog2);
+     
+    }
+    text("food remaining: "+e,250,200);
 }
 function readstock(data){
-foodS=data.val();
-}
-
+  foodS=data.val();
+  }
+  
+  
 function writestock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  e=x;
   database.ref('/').update({food:x});
+// text("food remaining: "+x,250,200);
 }
